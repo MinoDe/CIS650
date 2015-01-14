440d33dbd2bbae2d2c92ed4eba2e4538da805ae4
Danny Walinsky <dannywalinsky@gmail.com>
2015-01-13 21:09:29 -0800

    Updates to app2

--- app2.js
+++ app2.js
@@ -4,6 +4,7 @@ var express = require('express');
 var connect = require("connect");
 var blessed = require('blessed');
 var bodyParser = require('body-parser');
+var querystring = require('querystring');
 var app = express();
 
 app.use(bodyParser.urlencoded());
@@ -39,9 +40,9 @@ screen.append(box);
 
 app.set('port', process.env.PORT || 3000);
 
-var my_group = ["192.168.1.100", "192.168.1.106"];	// replace with real IPs of group
+var my_group = ["192.168.1.100", "192.168.1.102", "192.168.1.106", "192.168.1.107"];	// replace with real IPs of group
 
-var my_index = 1;	// replace with index of my IP in my_group
+var my_index = 0;	// replace with index of my IP in my_group
 
 box.setContent('this node (' + my_group[my_index] + ') will attempt to send its token to other nodes on network. ');
 screen.render();
@@ -56,78 +57,93 @@ app.get('/do_get', function (req, res){
 	res.json({"query": the_body, "id": JSON.stringify(my_group[my_index])});
 });
 
-isPrime = function(n) {
- if (isNaN(n) || !isFinite(n) || n%1 || n<2) return false; 
- if (n==leastFactor(n)) return true;
- return false;
-}
-
-leastFactor = function(n){
- if (isNaN(n) || !isFinite(n)) return NaN;  
- if (n==0) return 0;  
- if (n%1 || n*n<2) return 1;
- if (n%2==0) return 2;  
- if (n%3==0) return 3;  
- if (n%5==0) return 5;  
- var m = Math.sqrt(n);
- for (var i=7;i<=m;i+=30) {
-  if (n%i==0)      return i;
-  if (n%(i+4)==0)  return i+4;
-  if (n%(i+6)==0)  return i+6;
-  if (n%(i+10)==0) return i+10;
-  if (n%(i+12)==0) return i+12;
-  if (n%(i+16)==0) return i+16;
-  if (n%(i+22)==0) return i+22;
-  if (n%(i+24)==0) return i+24;
- }
- return n;
-}
 
 // handle POST requests
-function handlePost(req, res) {
+app.post('/do_post', function (req, res) {
 	var post_data = req.body;	//see connect package above
 	/*console.log ( "post body: " + the_body );
 	res.json({"body": the_body, "id": JSON.stringify(my_group[my_index])});*/
 
-	box.setContent("Count is at: " + post_data.n + ", with " + post_data.c + " primes found.");
+	//box.setContent("Count is at: " + post_data.n + ", with " + post_data.c + " primes found.");
+	countPrimes(post_data);
+	//box.style.bg = "green";
+	//screen.render();
+});
 
-	box.style.bg = 'red';
+function countPrimes(post_data) {
+	var kwork = post_data.k;
+	var num = post_data.n;
+	var count = post_data.c;
+
+	box.setContent("Running computations for " + post_data.k + " milliseconds");
+	box.style.bg = "green";
 	screen.render();
 
-	var start = new Date();
-	while(new Date().getTime() < start.getTime() + post_data.k) {
-		if(isPrime(post_data.n))
-			post_data.c++;
-		post_data.n++;
-	}
+	setTimeout(function() {
+	// Get Primes
+         var prevTime = (new Date()).getTime();
+         var status = 1;
 
-	box.style.bg = 'black';
+         while(1)
+     {
+         var curTime = (new Date()).getTime();
+                 var deltaTime = curTime - prevTime;
+
+                 if(deltaTime > kwork)
+                        break;
+         var j = 2;
+         for (  j = 2 ; j <= Math.sqrt(num) ; j++ )
+         {
+            if ( num%j == 0 )
+            {
+               status = 0;
+               break;
+            }
+         }
+         if ( status != 0 )
+         {
+            count++;
+         }
+         status = 1;
+         num++;
+
+
+      }
+
+	postToNext({c:count,n:num,k:kwork});
+	}, 0);
+}
+
+function postToNext(data) {
+	box.style.bg = "yellow";
+	box.setContent("Numbers looked at: " + data.n + ". Primes found: " + data.c);
 	screen.render();
-	var post_data = JSON.stringify(post_data);
+
+	var post_data = querystring.stringify(data);
 	var post_options = {
 		host: my_group[(my_index + 1) % my_group.length],
 		port: '3000',
 		path: '/do_post',
 		method: 'POST',
 		headers: {
-			'Content-Type': 'application/json',
+			'Content-Type': 'application/x-www-form-urlencoded',
 			'Content-Length': post_data.length
 		}
 	};
 	var post_req = http.request(post_options, function(res) {
 		res.setEncoding('utf8');
 		res.on('data', function (chunk) {
-			console.log('Response: ' + chunk);
+
 		});
 	});
 
 	// post the data
 	post_req.write(post_data);
 	post_req.end();
+
 }
-app.post('/do_post', handlePost);
 if(my_index == 0) {
-	handlePost({body: {n:0,c:0,k:5000}});
+	countPrimes({c:0,n:0,k:5000});
 }
 
 // Quit on Escape, q, or Control-C.
