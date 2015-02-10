b90a79e96506521bc1fcbebdd6479d1927e06776
Danny Walinsky <dannywalinsky@gmail.com>
2015-02-09 18:44:00 -0800

    IOT project 5

--- critical_section.js
+++ critical_section.js
@@ -35,38 +35,94 @@ box.focus();
 screen.render();
 
 var d = Discover();
-var node_addresses = [], timestamp = 0, pending_reply = false;
-var advertisement = {ip: ip.address(), ts: timestamp, request: false};
+var node_addresses = [], timestamp = 0, pending_reply = false, requesting = false, in_critical_section = false;
+var advertisement = {ip: ip.address(), ts: timestamp, request: false, votes: []};
 
 
 box.on('click', function() {
-	box.style.bg = "yellow";
-	screen.render();
-	advertisement.request = true;
-	d.advertise(advertisement);
-	pending_reply = node_addresses;
+	if(!requesting && !in_critical_section) {
+		requesting = true;
+		box.style.bg = "yellow";
+		box.setContent("Requesting resource: Timestamp is " + timestamp);
+		screen.render();
+		timestamp++;
+		advertisement.ts = timestamp;
+		advertisement.request = true;
+		d.advertise(advertisement);
+		pending_reply = node_addresses.slice();
+		if(node_addresses.length == 0) {
+			setTimeout(function() {startCriticalSection();}, 500);
+		}
+	}
+	if(in_critical_section) {
+		setTimeout(function() {
+			in_critical_section = false;
+			box.style.bg = "green";
+			box.setContent("Request resource");
+			screen.render();
+		}, 1000);
+	}
 });
 
 d.broadcast.on("hello", function (obj) {
 	if(obj.advertisement) {
-		if(obj.advertisement.ts > timestamp  || (obj.advertisement.ts == timestamp && obj.advertisement.ip > ip.address())) {
+		if(obj.advertisement.ts > timestamp)
 			timestamp = obj.advertisement.ts;
-			advertisement.reply = obj.advertisement.ip;
-			d.advertise(advertisement);
-		}
-		else if(advertisement.request == true) {
-			if(obj.advertisement.reply == ip.address()) {
-				
+		if(!in_critical_section) {
+			if(obj.advertisement.request == true && (!requesting || (obj.advertisement.ts < advertisement.ts  || (obj.advertisement.ts == advertisement.ts && obj.advertisement.ip > ip.address())))) {
+				// Vote for other node to proceed with critical section
+				voteFor(obj.advertisement);
+			}
+			else if(advertisement.request == true) {
+				// Search for this node's advertisement in the other node's votes
+				for(var i=0;i<obj.advertisement.votes.length;i++) {
+					if(obj.advertisement.votes[i].ip == ip.address() && obj.advertisement.votes[i].ts == advertisement.ts) {
+						checkPendingReplies(obj.advertisement.ip)
+					}
+				}
 			}
 		}
 	}
-
 });
+
+function voteFor(advertisement_object) {
+	for(var i=0;i<advertisement.votes.length;i++) {
+		if(advertisement.votes[i].ip == advertisement_object.ip) {
+			advertisement.votes.splice(i, 1);
+			break;
+		}
+	}
+	advertisement.votes.push({ip: advertisement_object.ip, ts: advertisement_object.ts});
+	d.advertise(advertisement);
+}
+
+function checkPendingReplies(ip_address) {
+	// Remove node from pending reply array
+	ip_index = pending_reply.indexOf(ip_address);
+	if(ip_index != -1) {
+		pending_reply.splice(ip_index, 1);
+		if(pending_reply.length == 0) {
+			// All votes have been cast! Start critical section.
+			startCriticalSection();
+		}
+	}
+}
+
+function startCriticalSection() {
+	in_critical_section = true;
+	requesting = false;
+	advertisement.request = false;
+	d.advertise(advertisement);
+	box.style.bg = "red";
+	box.setContent("Release resource");
+	screen.render();
+}
 
 d.on("added", function (obj) {
 	//console.log("A new node has been added: " + obj.address);
-	if(obj.address != ip.address())
+	if(obj.address != ip.address()) {
 		node_addresses.push(obj.address);
+	}
 });
 
 d.on("removed", function (obj) {
@@ -76,6 +132,9 @@ d.on("removed", function (obj) {
 			node_addresses.splice(i, 1);
 			break;
 		}
+	}
+	if(requesting) {
+		checkPendingReplies(obj.address);
 	}
 });
 
